ee4fba6834186de6e6109a4ce41771ff48680751
Jiaolong <45839824+Wang-Jiaolong@users.noreply.github.com>
2022-11-25 10:59:10 +0800

    a

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -33,7 +33,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/avatar.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    //repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -73,7 +73,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     pageButton: false, // 是否显示快捷翻页按钮，默认true
 
     // 默认外观模式（用户未在页面手动修改过模式时才生效，否则以用户设置的模式为准），可选：'auto' | 'light' | 'dark' | 'read'，默认'auto'。
-    // defaultMode: 'auto',
+     defaultMode: 'dark',
 
     // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
     sidebar: { mode: 'structuring', collapsable: false},
