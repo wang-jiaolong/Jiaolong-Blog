a8269e5a8ca2aa54092b7fe9a39a7fe3f9090952
Jiaolong <45839824+Wang-Jiaolong@users.noreply.github.com>
2023-03-24 16:25:09 +0800

    a

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -9,54 +9,54 @@ export default hopeTheme({
   hostname: "https://mister-hope.github.io",
 
   author: {
-    name: "Mr.Hope",
+    name: "Jiaolong",
     url: "https://mrhope.site",
   },
 
   iconAssets: "iconfont",
 
-  logo: "/logo.svg",
+  logo: "/logo.png",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  // repo: "vuepress-theme-hope/vuepress-theme-hope",
 
   docsDir: "docs",
 
-  blog: {
-    medias: {
-      Baidu: "https://example.com",
-      BiliBili: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
-      Email: "mailto:info@example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
-      Gitee: "https://example.com",
-      GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "mailto:info@example.com",
-      Instagram: "https://example.com",
-      Lark: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
-      Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
-      MrHope: ["https://mrhope.site", MR_HOPE_AVATAR],
-    },
-  },
+  // blog: {
+  //   medias: {
+  //     Baidu: "https://example.com",
+  //     BiliBili: "https://example.com",
+  //     Bitbucket: "https://example.com",
+  //     Dingding: "https://example.com",
+  //     Discord: "https://example.com",
+  //     Dribbble: "https://example.com",
+  //     Email: "mailto:info@example.com",
+  //     Evernote: "https://example.com",
+  //     Facebook: "https://example.com",
+  //     Flipboard: "https://example.com",
+  //     Gitee: "https://example.com",
+  //     GitHub: "https://example.com",
+  //     Gitlab: "https://example.com",
+  //     Gmail: "mailto:info@example.com",
+  //     Instagram: "https://example.com",
+  //     Lark: "https://example.com",
+  //     Lines: "https://example.com",
+  //     Linkedin: "https://example.com",
+  //     Pinterest: "https://example.com",
+  //     Pocket: "https://example.com",
+  //     QQ: "https://example.com",
+  //     Qzone: "https://example.com",
+  //     Reddit: "https://example.com",
+  //     Rss: "https://example.com",
+  //     Steam: "https://example.com",
+  //     Twitter: "https://example.com",
+  //     Wechat: "https://example.com",
+  //     Weibo: "https://example.com",
+  //     Whatsapp: "https://example.com",
+  //     Youtube: "https://example.com",
+  //     Zhihu: "https://example.com",
+  //     MrHope: ["https://mrhope.site", MR_HOPE_AVATAR],
+  //   },
+  // },
 
   locales: {
     "/": {
@@ -66,44 +66,16 @@ export default hopeTheme({
       // sidebar
       sidebar: enSidebar,
 
-      footer: "Default footer",
+      footer: "无限进步",
 
       displayFooter: true,
 
       blog: {
         description: "A FrontEnd programmer",
         intro: "/intro.html",
-      },
+      }
 
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
-    },
-
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
-      // navbar
-      navbar: zhNavbar,
-
-      // sidebar
-      sidebar: zhSidebar,
-
-      footer: "默认页脚",
-
-      displayFooter: true,
-
-      blog: {
-        description: "一个前端开发者",
-        intro: "/zh/intro.html",
-      },
-
-      // page meta
-      metaLocales: {
-        editLink: "在 GitHub 上编辑此页",
-      },
-    },
+    }
   },
 
   encrypt: {
@@ -114,12 +86,20 @@ export default hopeTheme({
   },
 
   plugins: {
+
+    components: {
+      // 你想使用的组件
+      components: [
+        "SiteInfo"
+      ],
+    },
+
     blog: true,
 
-    comment: {
-      // @ts-expect-error: You should generate and use your own comment service
-      provider: "Giscus",
-    },
+    // comment: {
+    //   // @ts-expect-error: You should generate and use your own comment service
+    //   provider: "Giscus",
+    // },
 
     // all features are enabled for demo, only preserve features you need here
     mdEnhance: {
