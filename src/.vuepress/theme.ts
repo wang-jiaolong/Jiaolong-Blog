e7c9d29de47b09475b5993462a76075d6fb3aa4d
Jiaolong <45839824+Wang-Jiaolong@users.noreply.github.com>
2023-05-07 17:09:28 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -25,38 +25,7 @@ export default hopeTheme({
 
   // blog: {
   //   medias: {
-  //     Baidu: "https://example.com",
-  //     BiliBili: "https://example.com",
-  //     Bitbucket: "https://example.com",
-  //     Dingding: "https://example.com",
-  //     Discord: "https://example.com",
-  //     Dribbble: "https://example.com",
-  //     Email: "mailto:info@example.com",
-  //     Evernote: "https://example.com",
-  //     Facebook: "https://example.com",
-  //     Flipboard: "https://example.com",
-  //     Gitee: "https://example.com",
-  //     GitHub: "https://example.com",
-  //     Gitlab: "https://example.com",
-  //     Gmail: "mailto:info@example.com",
-  //     Instagram: "https://example.com",
-  //     Lark: "https://example.com",
-  //     Lines: "https://example.com",
-  //     Linkedin: "https://example.com",
-  //     Pinterest: "https://example.com",
-  //     Pocket: "https://example.com",
-  //     QQ: "https://example.com",
-  //     Qzone: "https://example.com",
-  //     Reddit: "https://example.com",
-  //     Rss: "https://example.com",
-  //     Steam: "https://example.com",
-  //     Twitter: "https://example.com",
-  //     Wechat: "https://example.com",
-  //     Weibo: "https://example.com",
-  //     Whatsapp: "https://example.com",
-  //     Youtube: "https://example.com",
-  //     Zhihu: "https://example.com",
-  //     MrHope: ["https://mrhope.site", MR_HOPE_AVATAR],
+    
   //   },
   // },
 
@@ -77,6 +46,7 @@ export default hopeTheme({
       blog: {
         description: "A Solo Developer",
         intro: "/about",
+        articleInfo: ["Author","Date","PageView","Category","Word"]
       }
 
     }
@@ -103,7 +73,9 @@ export default hopeTheme({
     blog: {
       excerptLength: 50,
       filter: (page) => page.frontmatter.post,
-      sorter: (pageA, pageB) =>compareDate(pageA.frontmatter.date - pageB.frontmatter.date)
+      sorter: (pageA, pageB) =>compareDate(pageA.frontmatter.date - pageB.frontmatter.date),
+      articleInfo: ["Author","Date","PageView","Category","Word"]
+
     },
 
     // comment: {
