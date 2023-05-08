77bfa30054e1b037a144703c3edd286b24ae31c5
Jiaolong <45839824+Wang-Jiaolong@users.noreply.github.com>
2023-05-08 21:27:19 +0800

    Update en.ts

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   "/",
-  // { text: "Wiki", icon: "repo", link: "/wiki" },
+  { text: "Wiki", icon: "repo", link: "/wiki" },
   // { text: "Study", icon: "study", link: "/study" },
   { text: "Project", icon: "app", link: "/projects" },
   { text: "Gallery", icon: "pic", link: "/gallery" },
