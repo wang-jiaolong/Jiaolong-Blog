b77227fe979fb754a2d45bd580c8a7b60e617276
Jiaolong <376651763@qq.com>
2023-08-21 15:27:56 +0800

    Update BlogList.vue

--- docs/.vitepress/components/BlogList.vue
+++ docs/.vitepress/components/BlogList.vue
@@ -172,7 +172,7 @@ const props = defineProps({
 
                     .tag {
                         margin: 3px;
-                        padding: 1px 6px;
+                        padding: 0px 6px;
                         display: inline-block;
                         background: var(--vp-c-bg-soft-down);
                         font-size: 10px;
